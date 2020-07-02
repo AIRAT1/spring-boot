51f8670ef9f052488d04a3813b4f518878f02870
Ayrat <ayrat1@mail.ru>
2020-07-03 00:06:29 +0300

    spring boot app

--- src/main/java/de/example/boot/BootApplication.java
+++ src/main/java/de/example/boot/BootApplication.java
@@ -3,6 +3,7 @@ package de.example.boot;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BootApplication {
